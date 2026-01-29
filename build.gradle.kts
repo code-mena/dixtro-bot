0a81d6908abd44e7ae19d44854ba766e5664b0dc
lambda-mena <juan.cuevas.mena@gmail.com>
2026-01-29 22:14:19 +0000

    feat: upgrade youtube-source to 1.17.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("net.dv8tion:JDA:5.6.1")
 	implementation("dev.arbjerg:lavaplayer:2.2.6")
-	implementation("dev.lavalink.youtube:v2:1.16.0")
+	implementation("dev.lavalink.youtube:v2:1.17.0")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.projectlombok:lombok")
